5e11d7b24e12e292063c750ba3a06a64766baded
medbenzekri <medbenzekri219@gmail.com>
2022-11-19 23:26:53 +0100

    save/restore feature added but nothing works yet

--- main.py
+++ main.py
@@ -2,52 +2,70 @@ from time import sleep
 from simple_term_menu import TerminalMenu
 import keyboard
 from util import calculate
-n=3
-matrix=[[0,1,2,3,4,5,6,7,8,9],[10,20,25,30,40,45,40,50,60,55]]
-x=matrix[0]
-y=matrix[1]
-xory=TerminalMenu(["[x] append to x"," [y] append to y"])
+import pandas as pd 
+
+def fill(): 
+    global n
+    n= int(input("enter the size of your matrix eg: 5 -> matrix(5x5) : "))
+    listx= [float(input(f"entre the value x n°{i+1} :")) for i in range(0,n) ]
+    listy= [float(input(f"entre the value de y n°{i+1} :")) for i in range(0,n) ]
+    matrix.append(listx)
+    matrix.append(listy)
+
+if input("do you want ot use dummy values (y/n) ? ")=="y" :
+    matrix=[[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],[10, 20, 25, 30, 40, 45, 40, 50, 60, 55]]
+    x=matrix[0]
+    y=matrix[1]
+else:
+    n=0
+    matrix=[]
+    print("the matrix is empty !")
+    fill()
+    x=matrix[0]
+    y=matrix[1]
+
+    def insert_menu():
+        insert={"[1] fill the list": fill,
+            "[2]restore data":calculate.restore,
+            "[3] save data ":calculate.save
+        }
+        return insert
+
+    def plots():
+        plots={"[1] plot the function Dx/y":Calculate.plot_Dxy,
+               "[2] plot the function Dx/y":Calculate.plot_Dyx,
+               "[3] plot all ":Calculate.point_plot
+        }
+
 while(True):
     Calculate=calculate(x,y)
     append= lambda :[ list.append(matrix[i],float(input(f"enter the value you want to add to {t} : "))) for i,t in enumerate(["x","y"]) ]
     values = lambda: print(matrix)
-    def fill(): 
-        global n
-        n= int(input("enter the size of your matrix eg: 5 -> matrix(5x5) : "))
-        listx= [float(input(f"entre the value x n°{i+1} :")) for i in range(0,n) ]
-        listy= [float(input(f"entre the value de y n°{i+1} :")) for i in range(0,n) ]
-        matrix.append(listx,listy)
         
-
-    options = ["[a] add values to the matrix",
-            "[b] calculate the X̄", 
-            "[c] calculate the Ȳ ", 
-            "[d] fill the list",
-            "[e] list values",
-            "[f] calculate cov(x,y)",
-            "[g] calculate var(x)",
-            "[h] calculate var(y)",
-            "[i] calculate σ(x)",
-            "[j] calculate σ(y)",
-            "[k] calculate r(x,y)",
-            "[l] linear regresstion function ",
-            "[m] plot points",
-            "[n] exit "
-            ]
-    functions= [append,Calculate.moyen_x,
-                Calculate.moyen_y,fill,
-                values,Calculate.covxy,
-                Calculate.variance_x,Calculate.variance_y,Calculate.sdx,
-                Calculate.sdy,Calculate.r_status,
-                Calculate.regresstion_eq,
-                Calculate.point_plot,
-                exit]
+    options = {"[1] insert data":insert_menu,
+            # "[b] calculate the X̄":Calculate.moyen_x,
+            # "[c] calculate the Ȳ ":Calculate.moyen_y,
+            
+            # "[e] list values":values,
+            # "[f] calculate cov(x,y)":Calculate.covxy,
+            # "[g] calculate var(x)":Calculate.variance_x,
+            # "[h] calculate var(y)":Calculate.variance_y,
+            # "[i] calculate σ(x)":Calculate.sdx,
+            # "[j] calculate σ(y)":Calculate.sdy,
+            # "[k] calculate r(x,y)":Calculate.r_status,
+            "[2] regresstion function Dx/y": Calculate.regresstion_Dy_x,
+            "[3] regresstion function Dy/x":  Calculate.regresstion_Dx_y,
+            "[4] show results": Calculate.results,
+            "[5] draw plots":
+            "[e] exit ":exit
+    }
+    functions= options.keys()
     terminal_menu = TerminalMenu(options)
     index = terminal_menu.show()
     functions[index]()
     sleep(0.2)
-    input("Press esc to exit or any key to continue ...")
-    #key= keyboard.read_key()
+    print("Press esc to exit or any key to continue ...")
+    key= keyboard.read_key()
 
 
 
--- util.py
+++ util.py
@@ -1,10 +1,14 @@
+from cProfile import label
 import math
-from matplotlib.pyplot import plot,show
+from matplotlib.pyplot import plot,show,legend
 from numpy import array
+import pickle
 class calculate:
-    def __init__(self,x,y) -> None:
+    instance=None
+    def __init__(self,x,y):
+        if calculate.instance: return calculate.instance
         self.x=array(x)
-        self.y=array(y)
+        self.y=y
         self.n=len(x)
         self.x_bar=self.moyen(x)
         self.y_bar=self.moyen(y)
@@ -16,21 +20,55 @@ class calculate:
         self.standard_diviation_y=math.sqrt(self.var_y)
         #cofitient of corolation
         self.r=self.cov_xy/(self.standard_diviation_x*self.standard_diviation_y)
+        # values of a,b,α
         self.a=self.cov_xy/self.var_x
-        self.b=self.y_bar-self.a*self.y_bar
+        self.b=self.y_bar-self.a*self.x_bar
         self.y_function=self.a*self.x+self.b
+        self.alpha= self.cov_xy/self.var_y
+        self.beta=self.x_bar-self.alpha*self.y_bar
+        self.a_prime=1/self.alpha
+        self.b_prime=-self.beta/self.alpha
+        self.y_function_dxy=self.a_prime*self.x+self.b_prime
     def moyen(self,t):return sum(t)/len(t)
+
     def variance(self,t,m): return sum([xi**2 for xi in t ])/self.n -m**2 
+
     def co_variance(self): return sum([xi*yi for xi,yi in zip(self.x,self.y)])/self.n -(self.x_bar*self.y_bar)
-    # def co_variance(self): return sum([xi*yi for xi,yi in zip(self.x,self.y)])/self.n -(self.x_bar*self.y_bar)
+
     def moyen_x(self): print(f"X̄={self.x_bar:.2f}")
+
     def moyen_y(self):print(f"Ȳ={self.y_bar:.2f}")
+
     def variance_x(self): print(f"var(x)={self.var_x:.2f}")
+
     def variance_y(self): print(f"var(y)={self.var_y:.2f}")
+
     def covxy(self):print(f"cov(x,y)={self.cov_xy:.2f}")
     def sdx(self):print(f"σ(x)={self.standard_diviation_x:.2f}")
     def sdy(self):print(f"σ(y)={self.standard_diviation_y:.2f}")
     def r_status(self):print(f"""r is {self.r:.2f} so there is {f'a strong {"positive"if self.r>0 else "negative"} corolation' if 
                                                     abs(self.r)>=.5 else 'no corolation'  }""")
-    def regresstion_eq(self):print(f"Y={self.a:.2f}X{self.b:+.2f}")
-    def point_plot(self):plot(self.x,self.y,".");plot(self.x,self.y_function,"-");show()
+    def regresstion_Dy_x(self):return print(f"Y={self.a:.2f}X{self.b:+.2f}")
+    def regresstion_Dx_y(self):x=f"Y={self.a_prime:.2f}X{self.beta:+.2f}";print(x);return x
+    def point_plot(self):
+        plot(self.x,self.y,".")
+        plot(self.x,self.y_function,"-",label=self.regresstion_Dy_x)
+        plot(self.x,self.y_function_dxy,"-",label=self.regresstion_Dx_y)
+        legend(loc='upper left')
+        show()
+    def plot_Dxy(self):
+        
+    def plot_Dyx(self):    
+    def save(self):
+        with open("Data", 'wb') as file
+            pickle.dump(self,file)
+            file.close()
+    def load(name="Data"):
+        try:
+        with open(name,"rb") as file
+            return pickle.load(file)
+        except:
+            print("No previous saves ....!")
+    def restore():
+        calculate.instance=load()
+    
